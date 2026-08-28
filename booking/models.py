ffda7a02413f750d17fb237c235ce72368735617
Vova-Soloduhin <vovasoloduhin23@gmail.com>
2026-08-28 19:47:04 +0300

    added models Booking and Profile

--- booking/models.py
+++ booking/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Room(models.Model):
     name = models.CharField(max_length=100)
@@ -11,3 +12,31 @@ class Room(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone =  models.CharField(max_length=20, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+class Booking(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Очікує підтвердження'),
+        ('confirmed', 'Підтверджено'),
+        ('cancelled', 'Скасовано'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bookings')
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='bookings')
+
+    start_datatime = models.DateTimeField()
+    end_datatime = models.DateTimeField()
+
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.room.name}'
