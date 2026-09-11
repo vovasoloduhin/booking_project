1c322175b3ee5e402f55d0d07c10bf93506dc5dc
Vova-Soloduhin <vovasoloduhin23@gmail.com>
2026-09-11 19:37:34 +0300

    виправлено,тепер STATIC_URL є абсолютним

--- config/settings.py
+++ config/settings.py
@@ -64,8 +64,10 @@ TIME_ZONE = "Europe/Kyiv"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
