96c625236565ee864617737b1d3d322db72086c5
Vova-Soloduhin <vovasoloduhin23@gmail.com>
2026-09-25 20:36:38 +0300

    fix

--- config/settings.py
+++ config/settings.py
@@ -64,8 +64,9 @@ TIME_ZONE = "Europe/Kyiv"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
