770fc9d849add5ce8502fbb86cc85303ed229cd3
Vova-Soloduhin <vovasoloduhin23@gmail.com>
2026-09-25 20:33:31 +0300

    fix

--- config/settings.py
+++ config/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "django-insecure-change-this-in-production"
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
